b46d15b9c81a7aba590d5e0432fb529559b39d59
Julio Tavares <jctbs@cin.ufpe.br>
2021-11-30 15:10:38 -0300

    atualizaçao do title e deadline pronta

--- src/index.js
+++ src/index.js
@@ -63,12 +63,27 @@ app.post("/todos", checksExistsUserAccount, (request, response) => {
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo Not Found" });
+  }
+
+  todo.title = title;
+  todo.deadline = deadline;
+
+  return response.status(201).send();
 });
 
-app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+app.patch(
+  "/todos/:id/done",
+  checksExistsUserAccount,
+  (request, response) => {}
+);
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
   // Complete aqui
